9c64b0c649d8e3fd8d1bf051235d751e14146f9c
mbambroo1g <medha.bambroo@icloud.com>
2017-06-22 10:23:53 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,11 @@
 package com.fannie;
 //This is a new comment line
+//Added a new line from cloud
 
 class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Welcome to java");
+		System.out.println("Added new line");
+		
 		}
 	}
